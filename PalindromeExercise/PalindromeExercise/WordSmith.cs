da82054321c3a47afb3a917df6ccc3d1e6627a76
GitItHowYouLive <MicStandTerrence@gmail.com>
2022-05-06 01:27:59 -0500

    Initial Commit Method and Tests Complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var arr = word.ToCharArray();
+            Array.Reverse(arr);
+            var flip = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                flip += arr[i];
+            }
+            return flip == word;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("poop", true)]
+        [InlineData("boom", false)]
+        [InlineData("rush", false)]
+        [InlineData("iffi", true)]
+        [InlineData("racecar", true)]
 
+        public void PalTest(string word, bool expected)
+        {
+            //Arrange
+            var flip = new WordSmith();
+            //Act
+            var actual = flip.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(actual, expected);
         }
     }
 }
